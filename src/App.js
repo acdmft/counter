7e4835319f992635e4d204f26678d3a765861f53
fusbudget <andrewsanser@gmail.com>
2022-01-26 16:45:00 +0100

    add 2nd counter

--- src/App.js
+++ src/App.js
@@ -11,31 +11,18 @@ class App extends React.Component {
       count: 0
     };
 
-    this.increment = this.increment.bind(this);
-    this.substract = this.substract.bind(this);
+    
   }
   
-  increment() {
-    this.setState((prevState) => {
-      return { count: prevState.count + 1 };
-    });
-  }
-
-  substract() {
-    if (this.state.count > 0) {
-      this.setState((prevState) => {
-        return { count: prevState.count - 1 };  
-      });
-    }
-  }
-
+  
   render () {
     return (
       <div>
         <Counter 
-          count={this.state.count}
-          substract = {this.substract}
-          increment = {this.increment}
+          
+        >
+        </Counter>
+        <Counter 
         >
         </Counter>
       </div>
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,39 @@
 import React from "react";
 
 class Counter extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            count: 0
+        };
+        this.increment = this.increment.bind(this);
+        this.substract = this.substract.bind(this);
+    }
+    increment() {
+        this.setState((prevState) => {
+          return { count: prevState.count + 1 };
+        });
+    }
+    
+    substract() {
+        if (this.state.count > 0) {
+            this.setState((prevState) => {
+            return { count: prevState.count - 1 };  
+            });
+        }
+    }
+    
+
     render() {
         return (
             <div>
-                <h2>Count: {this.props.count}</h2>
+                <h2>Count: {this.state.count}</h2>
                 <div className="btn-wrapper">
-                    <button onClick={this.props.substract}>
+                    <button onClick={this.substract}>
                         -
                     </button>
-                    <button onClick={this.props.increment}>
+                    <button onClick={this.increment}>
                         +
                     </button>
                 </div>
